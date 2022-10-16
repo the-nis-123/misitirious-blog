c5dc07cd44afc3a53fa563ebe8443b50f26dd6ff
Kintu Denis <vienseydenis@gmail.com>
2022-10-16 23:53:14 +0300

    refactoring

--- src/axios.js
+++ src/axios.js
@@ -5,8 +5,3 @@ const BASE_URL = "http://localhost:3000";
 export default axios.create({
   baseURL: BASE_URL
 });
-
-export const axiosPrivate = axios.create({
-  baseURL: BASE_URL,
-  withCredentials: true
-});
