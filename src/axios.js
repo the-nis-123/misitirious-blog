8a29d4264ed61e88c761cd5e61885d368f6ebab1
kintu denis <kintuddenis@gmail.com>
2022-11-01 15:06:33 +0300

    hosted to github pages

--- src/axios.js
+++ src/axios.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const BASE_URL = "https://the-nis-123.github.io/fake-api/";
+const BASE_URL = "https://the-nis-123.github.io/fake-api/api/";
 
 export default axios.create({
   baseURL: BASE_URL
